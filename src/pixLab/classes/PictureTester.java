9d553305bf76428a339418aefa2857360985ee34
RyanPerkins <bobefett.qwert12@gmail.com>
2019-01-03 09:53:06 -0700

    added some more filters

--- src/pixLab/classes/PictureTester.java
+++ src/pixLab/classes/PictureTester.java
@@ -18,6 +18,14 @@ public class PictureTester
     beach.explore();
   }
   
+  public static void testZeroRed()
+  {
+    Picture beach = new Picture("beach.jpg");
+    beach.explore();
+    beach.zeroRed();
+    beach.explore();
+  }
+  
   /** Method to test mirrorVertical */
   public static void testMirrorVertical()
   {
