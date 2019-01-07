096d375e74105067b8a7312eb4f59856f839c339
RyanPerkins <bobefett.qwert12@gmail.com>
2019-01-07 08:15:12 -0700

    fixed getLargest and getColTotal so they pass the tests

--- src/pixLab/classes/IntArrayWorker.java
+++ src/pixLab/classes/IntArrayWorker.java
@@ -100,4 +100,22 @@ public class IntArrayWorker
     }
   }
  
+  public int getLargest() {
+	  int largest = Integer.MIN_VALUE;
+	  for (int row = 0; row < matrix.length; row++) {
+		  for (int col = 0; col < matrix[0].length; col++) {
+			  if (matrix[row][col] >= largest) {
+				  largest = matrix[row][col];
+			  }
+		  }
+	  }
+	  return largest;
+  }
+  public int getColTotal(int col) {
+	  int total = 0;
+	  for (int i = 0; i < matrix.length; i++) {
+		  total += matrix[i][col];
+	  }
+	  return total;
+  }
 }
